f1fe25ba21c115be96a8bd5868de344a21e70be2
saurabhkumar998 <saurabh.kumar.2382000@gmail.com>
2023-01-19 01:27:22 +0530

    configured Spring JPA and CommandLineRunner

--- jpa-and-hibernate/src/main/java/com/tutorial/springboot/jpaandhibernate/course/Course.java
+++ jpa-and-hibernate/src/main/java/com/tutorial/springboot/jpaandhibernate/course/Course.java
@@ -1,9 +1,16 @@
 package com.tutorial.springboot.jpaandhibernate.course;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
 	
+	@Id
 	private long id;
+	
 	private String name;
+	
 	private String author;
 	
 	public Course() {
--- jpa-and-hibernate/src/main/java/com/tutorial/springboot/jpaandhibernate/course/CourseCommandLineRunner.java
+++ jpa-and-hibernate/src/main/java/com/tutorial/springboot/jpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,21 +1,23 @@
-package com.tutorial.springboot.jpaandhibernate.course.jdbc;
+package com.tutorial.springboot.jpaandhibernate.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.tutorial.springboot.jpaandhibernate.course.Course;
+import com.tutorial.springboot.jpaandhibernate.course.jpa.CourseJpaRepository;
 /*
  * This class implements the CommandLineRunner interface, which is used to run a block of code(method) at the start of a spring boot application (at startup)
  *
  *This method will run the  insert method of the CourseJdbcRepository class to insert data in the H2 database at the start of the application 
  * */
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+public class CourseCommandLineRunner implements CommandLineRunner {
 
-	@Autowired
-	private CourseJdbcRepository repository;
+//	@Autowired
+//	private CourseJdbcRepository repository;
 	
+	@Autowired
+	private CourseJpaRepository repository;
 	
 	@Override
 	public void run(String... args) throws Exception {
